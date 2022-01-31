0eadedf38fa47a06e88cdabaf8a6c1ea2817c9c6
Tanvir Mohammad Rizvy <tanvirmohammad2@gmail.com>
2022-01-31 14:47:55 +0600

    updated

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ const mongoose = require("mongoose");
 const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
 const postRoute = require("./routes/posts");
+const categoryRoute = require('./routes/categories');
 const multer = require("multer");
 
 
@@ -18,12 +19,23 @@ mongoose.connect(process.env.MONGO_URL, {
   .catch((err) => console.log(err));
 
   const storage = multer.diskStorage({
-    
+    destination:(req,file,cb) => {
+      cb(null,"image");
+    },
+    filename: (req,file,cb) => {
+      cb(null, req.body.name);
+    },
+  });
+
+  const upload = multer({storage:storage});
+  app.post("/api/upload", upload.single("file"), (req,res) => {
+    res.status(200).json("file has been uploaded");
   });
 
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/post", postRoute);
+app.use("/api/categories", categoryRoute);
 
 
 
